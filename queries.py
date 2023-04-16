bc47b4f2fb06989d644097107df7ca59c6f4c23f
Syed Haider Ali Zaidi <72228466+syedhaideralizaidi@users.noreply.github.com>
2023-04-16 22:03:11 +0500

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,8 @@
 import boto3
 
 # create a DynamoDB client object
+from boto3.dynamodb.conditions import Key
+
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', aws_access_key_id='AKIAXFLZELRT5YARSFUC', aws_secret_access_key='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')
 
 table = dynamodb.Table('BitcoinTweet')
@@ -42,4 +44,48 @@ def query2():
 	for tweet in tweets:
 	    print(tweet)
 
-query2()
+def query3():
+	threshold = 1
+	response = table.scan(
+		FilterExpression='user_followers > :val',
+		ExpressionAttributeValues={
+			':val': threshold
+		}
+	)
+	sorted_items = sorted(response['Items'], key=lambda x: x['user_followers'], reverse=True)
+	k = 10
+	top_k_users = sorted_items[:k]
+
+	# Print the top k users
+	for user in top_k_users:
+		print(user['user_id'])
+
+
+def query4():
+	threshold = 1
+	response = table.scan(
+		FilterExpression='user_followers > :val',
+		ExpressionAttributeValues={
+			':val': threshold
+		}
+	)
+	sorted_items = sorted(response['Items'], key=lambda x: x['user_followers'], reverse=True)
+	k = 10
+	top_k_users = sorted_items[:k]
+
+	# Print the top k users
+	for user in top_k_users:
+		print(user['tweet_id'])
+
+def query6():
+	threshold = 2
+	response = table.scan(
+		FilterExpression='user_followers < :val',
+		ExpressionAttributeValues={
+			':val': threshold
+		}
+	)
+	for item in response['Items']:
+		print("Deleting tweets for user with followers : ", item['user_followers'])
+
+query6()
