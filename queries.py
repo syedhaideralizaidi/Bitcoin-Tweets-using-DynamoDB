ba95c14e27dd6a4a4d092d99e4faedc9ffdcf0a4
Syed Haider Ali Zaidi <72228466+syedhaideralizaidi@users.noreply.github.com>
2023-04-16 22:33:53 +0500

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,4 @@
 import boto3
-
-# create a DynamoDB client object
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', aws_access_key_id='AKIAXFLZELRT5YARSFUC', aws_secret_access_key='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')
@@ -19,7 +17,7 @@ def query1():
 	    print(item['tweet_id'])
 
 def query2():
-	location = 'NOVA' # specify the location to search for
+	location = 'NOVA' 
 
 	response = table.scan(
 	    FilterExpression='user_location = :val',
@@ -40,7 +38,7 @@ def query2():
 	    )
 	    tweets.extend(response['Items'])
 
-	# print the results
+	
 	for tweet in tweets:
 	    print(tweet)
 
@@ -56,7 +54,7 @@ def query3():
 	k = 10
 	top_k_users = sorted_items[:k]
 
-	# Print the top k users
+
 	for user in top_k_users:
 		print(user['user_id'])
 
@@ -73,9 +71,33 @@ def query4():
 	k = 10
 	top_k_users = sorted_items[:k]
 
-	# Print the top k users
 	for user in top_k_users:
 		print(user['tweet_id'])
+
+def query5():
+	desired_hashtags = {'bitcoin', 'crypto', 'blockchain'}
+	response = dynamodb.scan(
+    TableName=table_name,
+    FilterExpression='contains(hashtags, :h1) or contains(hashtags, :h2) or contains(hashtags, :h3)',
+    ExpressionAttributeValues={
+        ':h1': {'S': 'bitcoin'},
+        ':h2': {'S': 'crypto'},
+        ':h3': {'S': 'blockchain'}
+    	}
+	)
+
+	tweet_count = {}
+	for item in response['Items']:
+	    tweet_id = item['tweet_id']['S']
+	    if tweet_id in tweet_count:
+	        tweet_count[tweet_id] += 1
+	    else:
+	        tweet_count[tweet_id] = 1
+
+	top_k_tweets = sorted(tweet_count.items(), key=lambda x: x[1], reverse=True)[:k]
+
+	for tweet_id, count in top_k_tweets:
+	    print(f'Tweet ID: {tweet_id}, Frequency: {count}')		
 
 def query6():
 	threshold = 2
